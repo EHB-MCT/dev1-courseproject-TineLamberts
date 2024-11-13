5ad9481d3910c2e08ff212d1a6b3e191afec436d
Tine Lamberts <tine.lamberts@student.ehb.be>
2024-11-13 17:31:13 +0100

    Draw different cirkles

---
+++ milestone2-generative-design.js
@@ -0,0 +1,37 @@
+"use strict";
+
+import context from "../../Scripts/context.js";
+import * as Utils from "../../Scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+// Coordinates for the cross lines
+let x = width / 2;
+let y = height / 2;
+
+// First draw the background circles
+drawRandomCircles();
+drawLine();
+
+function drawLine() {
+	context.lineWidth = 50;
+	context.strokeStyle = "black";
+	Utils.drawLine(x, 0, x, height);
+	Utils.drawLine(0, y, width, y);
+}
+
+function drawRandomCircles() {
+	// Set a fill color for the background
+	context.fillStyle = "white";
+	context.fillRect(0, 0, width, height); // Clear and fill the canvas
+
+	for (let i = 0; i < 10000; i++) {
+		let x = Math.random() * width;
+		let y = Math.random() * height;
+
+		context.fillStyle = Utils.hsl(Math.random() * 360, 50, 50);
+
+		Utils.fillCircle(x, y, 10);
+	}
+}
