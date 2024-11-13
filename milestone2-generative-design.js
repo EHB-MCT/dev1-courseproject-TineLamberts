16f15943d0868dafb59fcbc54588ac8acd4ace20
Tine Lamberts <tine.lamberts@student.ehb.be>
2024-11-13 18:35:15 +0100

    Milestone 2 DONE

--- milestone2-generative-design.js
+++ milestone2-generative-design.js
@@ -6,26 +6,23 @@ import * as Utils from "../../Scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-// Coordinates for the cross lines
 let x = width;
 let y = height;
 
-// First draw the background circles
 drawRandomCircles();
 drawLine();
 
 function drawLine() {
 	for (let x = 0, y = 0; x < width || y < height; x += 100, y += 100) {
 		context.lineWidth = 50;
-		Utils.drawLine(-25, y, width, y);
-		Utils.drawLine(x, y, x, height);
+		Utils.drawLine(x, -25, width, y);
+		Utils.drawLine(-25, y, x, height);
 	}
 }
 
 function drawRandomCircles() {
-	// Set a fill color for the background
 	context.fillStyle = "white";
-	context.fillRect(0, 0, width, height); // Clear and fill the canvas
+	context.fillRect(0, 0, width, height);
 
 	for (let i = 0; i < 1000; i++) {
 		let x = Math.random() * width;
