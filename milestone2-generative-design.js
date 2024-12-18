4409dbc6aea48364e571c629823f6e2a875b1188
Tine Lamberts <tine.lamberts@student.ehb.be>
2024-12-18 16:31:43 +0100

    Generative design

--- milestone2-generative-design.js
+++ milestone2-generative-design.js
@@ -7,24 +7,31 @@ let width = context.canvas.width;
 let height = context.canvas.height;
 
 window.onmousedown = click;
+window.onmousemove = move;
 
 let x = width;
 let y = height;
 
-drawLine();
-drawRandomCircles();
-
+/**
+ *
+ * @param {MouseEvent} eventData
+ */
+function move(evenData) {
+	let x = evenData.pageX;
+	let y = evenData.pageY;
+	drawRandomCircles();
+}
 function drawRandomCircles() {
 	context.fillStyle = "white";
 	context.fillRect(0, 0, width, height);
 
-	for (let i = 0; i < 1000; i++) {
+	for (let i = 0; i < 500; i++) {
 		let x = Math.random() * width;
 		let y = Math.random() * height;
 
 		context.fillStyle = Utils.hsl(Math.random() * 360, 50, 50);
 
-		Utils.fillCircle(x, y, 45);
+		Utils.fillCircle(x, y, 30);
 	}
 }
 
@@ -41,7 +48,6 @@ function click(e) {
 function drawLine() {
 	for (let x = 0, y = 0; x < width || y < height; x += 100, y += 100) {
 		context.lineWidth = 50;
-		//kleiner maken
 		Utils.drawLine(x, 0, width, y);
 		Utils.drawLine(0, y, x, height);
 	}
